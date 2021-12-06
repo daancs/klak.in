24e4e6fab20e5dcef6dc316a6cb186ca26abc8b8
daancs <dadi.andrason@gmail.com>
2021-12-06 21:59:55 +0100

    added stuff

--- alohomora/src/services/unlock.service.js
+++ alohomora/src/services/unlock.service.js
@@ -116,6 +116,8 @@ function getAptusUrlFromPayload(payload) {
   const urlParts = JSON.parse(payload.substring(7, payload.length - 2)).data[
     'aptuslogin@APTUSPORT'
   ].objekt[0].aptusUrl.split(' ')
+  
+  console.log(urlParts)
   return `${urlParts[0]}%20${urlParts[1]}`
 }
 
@@ -128,9 +130,9 @@ async function unlockDoor(doorID = 116402) {
     const aptusCookie = await getAptusCookies(aptusUrl)
     console.log('--------------------------')
     const unlockMsg = await _unlockDoor(aptusCookie, doorID)
-    if (unlockMsg !== '{"StatusText":"Dörren är upplåst","HeaderStatusText":"Status"}') {
+    if (unlockMsg !== '{"StatusText":"Door is unlocked","HeaderStatusText":"Status"}') {
       console.log(
-        `Bad return body,\nexpect: "{"StatusText":"Dörren är upplåst","HeaderStatusText":"Status"}"\ngot: ${unlockMsg}`,
+        `Bad return body,\nexpect: "{"StatusText":"Door is unlocked","HeaderStatusText":"Status"}"\ngot: ${unlockMsg}`,
       )
       return { success: false }
     }
