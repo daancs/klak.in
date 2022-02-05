a9d384db3bcb90a790d4e3b05f7c62a58cc1cf50
daancs <dadi.andrason@gmail.com>
2022-02-05 22:37:24 +0100

    removed some redundant code

--- alohomora/src/router.js
+++ alohomora/src/router.js
@@ -1,10 +1,7 @@
 const router = require('express').Router()
-const { route } = require('.')
 const { unlock } = require('./controllers/openings.controller')
 
 router.get('/unlock/:id', unlock)
-// create a route for the unlock endpoint with an ID param
-//router.route('/unlock/:id').get(unlock)
 
 
 module.exports = router
