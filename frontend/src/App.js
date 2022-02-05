8dc0935193a2112e58775a8d1f9efd9ff9b5a9b0
daancs <dadi.andrason@gmail.com>
2022-02-05 16:44:44 +0100

    added way to load in several doors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,49 @@
 import React from 'react'
 import './App.css'
 import RequestHOC from './HOC/Request'
-import OpenDoorView from './Components/OpenDoorView'
+//import OpenDoorView from './Components/OpenDoorView'
 import LoaderView from './Components/LoaderView'
 import ResultView from './Components/ResultView'
 import AboutMeView from './Components/AboutMeView'
 import LockCardView from './Components/LockCardView'
+// import doors.json as jsonfile
+import doors from './doors.json'
 
 function App() {
+
+  let chaboDoors = JSON.parse(JSON.stringify(doors));
+  console.log(chaboDoors);
+
+  let map = new Map();
+  map.set(116400, "Kemivägen 7B");
+  map.set(116402, "Kemivägen 7A");
+
   return (
     <div className="divide">
       <div className="cover-all">
-      <RequestHOC>
-        {(openDoor, loading, res) => {
-          if (res !== undefined) {
-            return <ResultView res={res} />
-          } else if (loading) {
-            return <LoaderView />
-          }
-          return <OpenDoorView openDoor={openDoor} />
-        }}
-      </RequestHOC>
+        <RequestHOC>
+          {(openDoor, loading, res) => {
+            if (res !== undefined) {
+              return <ResultView res={res} />
+            } else if (loading) {
+              return <LoaderView />
+            }
+            return Array.from(chaboDoors).map(([doorID, name]) => (
+              <LockCardView key={doorID} openDoor={openDoor} doorID={doorID} name={name} />
+            ))
+            /*return Array.from(map).map(([id, name]) => {
+              return <LockCardView key={id} openDoor={openDoor} doorID={id} name={name} />
+            })*/
+          //return <LockCardView openDoor={openDoor, 116400} /> //<OpenDoorView openDoor={openDoor} />
+        }
+        }
+        </RequestHOC>
       </div>
       <div className="about-me">
         <AboutMeView />
       </div>
     </div>
-  
+
   )
 }
 
