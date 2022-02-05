a4f2ade17ec34d1c4afce7fcc2c2144dccee96a8
daancs <dadi.andrason@gmail.com>
2022-02-05 19:03:41 +0100

    parsed json into map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,40 +11,40 @@ import doors from './doors.json'
 
 function App() {
 
-  let chaboDoors = JSON.parse(JSON.stringify(doors));
-  console.log(chaboDoors);
+	let jsonparse = JSON.parse(JSON.stringify(doors));
+	
+	let map = new Map();
+	jsonparse.doors.forEach(door => {
+		map.set(door.doorID, door.name);
+	});
 
-  let map = new Map();
-  map.set(116400, "Kemivägen 7B");
-  map.set(116402, "Kemivägen 7A");
+	return (
+		<div className="divide">
+			<div className="cover-all">
+				<RequestHOC>
+					{(openDoor, loading, res) => {
+						if (res !== undefined) {
+							return <ResultView res={res} />
+						} else if (loading) {
+							return <LoaderView />
+						}
+						/*return Array.from(chaboDoors).map(([doorID, name]) => (
+						  <LockCardView key={doorID} openDoor={openDoor} doorID={doorID} name={name} />
+						))*/
+						return Array.from(map).map(([id, name]) => {
+							return <LockCardView key={id} openDoor={openDoor} doorID={id} name={name} />
+						})
+						//return <LockCardView openDoor={openDoor, 116400} /> //<OpenDoorView openDoor={openDoor} />
+					}
+					}
+				</RequestHOC>
+			</div>
+			<div className="about-me">
+				<AboutMeView />
+			</div>
+		</div>
 
-  return (
-    <div className="divide">
-      <div className="cover-all">
-        <RequestHOC>
-          {(openDoor, loading, res) => {
-            if (res !== undefined) {
-              return <ResultView res={res} />
-            } else if (loading) {
-              return <LoaderView />
-            }
-            return Array.from(chaboDoors).map(([doorID, name]) => (
-              <LockCardView key={doorID} openDoor={openDoor} doorID={doorID} name={name} />
-            ))
-            /*return Array.from(map).map(([id, name]) => {
-              return <LockCardView key={id} openDoor={openDoor} doorID={id} name={name} />
-            })*/
-          //return <LockCardView openDoor={openDoor, 116400} /> //<OpenDoorView openDoor={openDoor} />
-        }
-        }
-        </RequestHOC>
-      </div>
-      <div className="about-me">
-        <AboutMeView />
-      </div>
-    </div>
-
-  )
+	)
 }
 
 export default App
