4353b55643facf17f6f061d39521470f7e630db0
daancs <dadi.andrason@gmail.com>
2022-02-05 17:11:51 +0100

    fixing git pls work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import RequestHOC from './HOC/Request'
 //import OpenDoorView from './Components/OpenDoorView'
 import LoaderView from './Components/LoaderView'
 import ResultView from './Components/ResultView'
-<<<<<<< HEAD
 import AboutMeView from './Components/AboutMeView'
 import LockCardView from './Components/LockCardView'
 // import doors.json as jsonfile
@@ -46,29 +45,6 @@ function App() {
     </div>
 
   )
-=======
-
-function App() {
-	return (
-		<div className="divide">
-			<div className="cover-all">
-				<RequestHOC>
-					{(openDoor, loading, res) => {
-						if (res !== undefined) {
-							return <ResultView res={res} />
-						} else if (loading) {
-							return <LoaderView />
-						}
-						return <OpenDoorView openDoor={openDoor} />
-					}}
-				</RequestHOC>
-			</div>
-			{/*<div className="about-me">
-        <AboutMeView />
-      </div>*/}
-		</div>
-	)
->>>>>>> master
 }
 
 export default App
